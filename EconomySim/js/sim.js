d34b2998be502723a8593b5204fc3c14e3a3adf2
FromRust <49226362+FromRust@users.noreply.github.com>
2019-04-06 18:16:11 -0700

    Update sim.js

--- EconomySim/js/sim.js
+++ EconomySim/js/sim.js
@@ -11,6 +11,8 @@ var onlySellDuplicates = false;
 
 // sim results data
 var totalMissions = 0;
+var totalMissionsWon = 0;
+var totalArray = [];
 
 // card collection data
 // structure: {id: {numOwned}}
@@ -42,37 +44,60 @@ function gatherData() {
 }
 
 function runSims() {
-  // reset values
-  numWinsFirst = 0;
-  numCardsInPool = 0;
-  numCardsOnMissionSuccess = 1;
-  missionSuccessPercent = 50;
-  monsterCardCost = 500;
-  abilityCardCost = 500;
-  characterCardCost = 500;
-  cardRefundAmount = 100;
-  onlySellDuplicates = false;
-  totalMissions = 0;
+  totalArray = [];
 
-  cardCollection = {};
-
-  // gather data from form
-  gatherData();
-
-  // run the sims
-  for(var x = 0; x < numSims; x++)
+  for(var s = 0; s < numSims; s++)
   {
-    runSingleSim();
+    // for each sim, we...
+    // reset values
+    var safetyCheck = 10000; // we dont do more than this many runs
+    var stopCondition = false;
+
+    numCardsInPool = 0;
+    numCardsOnMissionSuccess = 1;
+    missionSuccessPercent = 50;
+    monsterCardCost = 500;
+    abilityCardCost = 500;
+    characterCardCost = 500;
+    cardRefundAmount = 100;
+    onlySellDuplicates = false;
+    totalMissions = 0;
+    totalMissionsWon = 0;
+
+    cardCollection = {};
+
+    // gather data from form
+    gatherData(); // this is inefficient - we only need to do this once. TODO: refactor this
+
+    // run the missions
+    for(var x = 0; (x < safetyCheck) && !stopCondition; x++)
+    {
+      stopCondition = runSingleSim();
+    }
+
+    var innerHTMLString = "";
+    if(stopCondition)
+    {
+      // we hit our actual condition instead of a safety check, so
+      // count this sim
+      // not sure what to do with safety check sims but we'll figure that out later
+      totalArray.push(totalMissions);
+    }
+    else
+    {
+      totalArray.push(safetyCheck);
+    }
   }
 
-  var innerHTMLString = "";
-  for(var x = 0; x < numCardsInPool; x++)
-  {
-    var numCards = getNumCardsOwned(x);
-    innerHTMLString += "" + numCards + x + ": " + numCards +"<br />";
-  }
+  // display results
+  displayResults();
 
-  document.getElementById("result").innerHTML = innerHTMLString;
+  /*innerHTMLString += "Total missions: " + totalMissions + "<br />";
+  innerHTMLString += "Total missions won: " + totalMissionsWon + "<br />";
+
+  innerHTMLString += getCardCollectionString();
+
+  document.getElementById("result").innerHTML = innerHTMLString;*/
 
   // gather up results
   /*
@@ -88,18 +113,66 @@ function runSims() {
   */
 }
 
+function displayResults()
+{
+  var innerHTMLString = "";
+  innerHTMLString += "Average missions to get 1 specific card: " + getAvgMissions() + "<br />";
+  document.getElementById("result").innerHTML = innerHTMLString;
+}
+
+function getAvgMissions()
+{
+  var total = 0;
+  for(var x = 0; x < totalArray.length; x++)
+  {
+    total += totalArray[x];
+  }
+
+  total /= numSims;
+  return total;
+}
+
+function getCardCollectionString()
+{
+  var string = "";
+  for(var x = 0; x < numCardsInPool; x++)
+  {
+    var numCards = getNumCardsOwned(x);
+    if(numCards > 0)
+    {
+      string += "" + x + ": " + numCards + "<br />";
+    }
+  }
+
+  return string;
+}
+
 function runSingleSim() {  
   // was the mission a success?
   if(runMission())
   {
     // it was! give 'em a card
-    getCard(selectCardId());
+    totalMissionsWon++;
+    for(var x = 0; x < numCardsOnMissionSuccess; x++)
+    {
+      getCard(selectCardId());
+    }
   }
+
+  //check for condition
+  return checkStopCondition();
+}
+
+function checkStopCondition()
+{
+  // for testing, we just check to see if we have card ID #77
+  return (getNumCardsOwned(77) > 0);
 }
 
 function runMission()
 {
-  return Math.floor(Math.random() * 101) >= missionSuccessPercent;
+  totalMissions++;
+  return (Math.floor(Math.random() * 101) <= missionSuccessPercent);
 }
 
 function selectCardId()
